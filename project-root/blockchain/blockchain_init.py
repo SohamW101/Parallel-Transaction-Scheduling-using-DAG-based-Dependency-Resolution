208e90de6626f29753b86b37f4f41c479d6b7c0d
Divaksh <b24ee1095@iitj.ac.in>
2025-11-14 01:03:50 +0530

    Removed redundant files

--- project-root/blockchain/blockchain_init.py
+++
@@ -1,9 +0,0 @@
-# blockchain/__init__.py
-"""Blockchain module for DiPETrans framework"""
-
-from .transaction import Transaction
-from .block import Block
-from .blockchain import Blockchain
-from .crypto_utils import hash_data, verify_hash, generate_address
-
-__all__ = ['Transaction', 'Block', 'Blockchain', 'hash_data', 'verify_hash', 'generate_address']
